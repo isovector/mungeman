832da79c4e7ca59f438d53b3d614b9c7a55ff645
Sandy Maguire <sandy@sandymaguire.me>
2019-06-28 13:41:06 -0400

    setstore + redisutils

--- src/Polysemy/KVStore/Redis.hs
+++ src/Polysemy/KVStore/Redis.hs
@@ -7,44 +7,11 @@ module Polysemy.KVStore.Redis
 
 import           Control.Monad
 import           Data.Binary (Binary)
-import qualified Data.Binary as B
-import           Data.Binary.Get (runGet)
-import           Data.Binary.Put (runPut)
-import           Data.ByteString (ByteString)
-import qualified Data.ByteString.Lazy.Char8 as L
 import qualified Database.Redis as R
-import           GHC.Exts
 import           Polysemy
 import           Polysemy.Error
 import           Polysemy.KVStore
-
-
-newtype Path = Path { getPath :: ByteString }
-  deriving (Eq, Ord, Show, IsString)
-
-
--- TODO(sandy): base lib
-hoistError
-    :: Member (Error e) r
-    => Either e a
-    -> Sem r a
-hoistError (Left e) = throw e
-hoistError (Right a) = pure a
-
-sendMError
-    :: ( Member (Error e) r
-       , Member (Lift m) r
-       )
-    => m (Either e a)
-    -> Sem r a
-sendMError = hoistError <=< sendM
-
-
-putForRedis :: Binary a => a -> ByteString
-putForRedis = L.toStrict . runPut . B.put
-
-getFromRedis :: Binary a => ByteString -> a
-getFromRedis = runGet B.get . L.fromStrict
+import           Polysemy.RedisUtils
 
 
 runKVStoreInRedis
